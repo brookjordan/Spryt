3a31388df09a68bebb0eb2a238ad2601af7fc10a
brookjordan <brkjrdn@gmail.com>
2016-02-01 19:31:52 +0000

    Get the file picker looking good
    It's kinda hard to style the default browser file
    pickers so I need to add a wrapper on it.

--- project/src/es6/entry/main.js
+++ project/src/es6/entry/main.js
@@ -1,4 +1,7 @@
 import drawerBuilder     from "../component/togglable.js";
+import filePickerBuilder from "../component/file-picker.js";
 
 drawerBuilder.init('spryt-sheets');
 drawerBuilder.init('spryt-animations');
+
+filePickerBuilder.init('spryt-map-picker');
---
+++ project/src/es6/method/randomString.js
@@ -0,0 +1,13 @@
+const startLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const otherLetters = '0123456789---___'.split('');
+const allLetters   = startLetters.concat(otherLetters);
+
+export default length => {
+  let str = '';
+  for (let i = length; i > 0; --i) {
+    str += str.length ?
+      allLetters[Math.floor(Math.random() * allLetters.length)]:
+      startLetters[Math.floor(Math.random() * startLetters.length)];
+  }
+  return str;
+};
